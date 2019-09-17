b66a02f5c8cb333ad3a5a91ecc09aac88fe655d8
ktsiouni <ktsiouni@PCE-17.uwo.ca>
2019-09-17 18:28:27 -0400

    Changes in main methods providing info about the constructor parameters.

--- src/main/java/ATMMain.java
+++ src/main/java/ATMMain.java
@@ -1,3 +1,5 @@
+import java.net.InetAddress;
+
 import atm.ATM;
 import banking.Money;
 
@@ -5,7 +7,38 @@ public class ATMMain {
 
 	public static void main(String[] args) {
 		try {
-			ATM theATM = new ATM(0, "London", "CIBC Branch", null, true, 1234, "1234", 0, 0, 1, new Money(20));
+			// Bank information. Important only for the receipt.
+			int bankID = 0;
+			String location = "London";
+			String bankName = "CIBC Branch";
+			InetAddress bankAddress = null;
+			
+			// Parameters used when testing the app.
+			boolean testing = true;
+			int cardID = 6789;
+			String pin = "678";
+			// Use to choose type of transaction.
+			// (0 = withdrawal, 1 = deposit, 2 = transfer, 3 = inquiry)
+			int transactionChoice = 0;
+			// Choose accounts the money are coming from or going to.
+			// (0 = checking, 1 = savings, 2 = money market)
+			int fromAccount = 0;
+			int toAccount = 1;
+			// The amount of that particular transaction.
+			Money amount = new Money(20);
+			ATM theATM = new ATM(
+								 bankID, 
+								 location, 
+								 bankName, 
+								 bankAddress, 
+								 testing, 
+								 cardID, 
+								 pin, 
+								 transactionChoice, 
+								 fromAccount, 
+								 toAccount, 
+								 amount
+								);
 
 			new Thread(theATM).start();
 
@@ -16,7 +49,7 @@ public class ATMMain {
 
 			Thread.sleep(5000);
 
-			System.out.println("Set initial cash available.");
+			System.out.println("Set initial cash available to the machine.");
 			theATM.getCashDispenser().setInitialCash(new Money(2000));
 			System.out.println("Connect to bank.");
 			theATM.getNetworkToBank().openConnection();
